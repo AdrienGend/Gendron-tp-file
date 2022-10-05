5f4dc572cafd0342bbfc692c97ab56c80bdf1be3
AdrienGendron <112941884+AdrienGend@users.noreply.github.com>
2022-10-05 17:35:39 +0200

    tp fini

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	data, _ := os.ReadFile("Hangman.txt")
+
+	var tableau []string
+	var word string
+
+	for index, char := range data {
+		if char == 13 {
+			tableau = append(tableau, word)
+			word = ""
+		} else if char != 10 {
+			word += string(char)
+		}
+		if index == len(data)-1 {
+			tableau = append(tableau, word)
+		}
+	}
+
+	fmt.Println(tableau[0])
+
+	fmt.Println(tableau[len(tableau)-1])
+	var a string
+	for i := 1; i < len(tableau); i++ {
+		if tableau[i-1] == "before" {
+			a = tableau[i]
+			// tmp, _ := strconv.Atoi(tableau[i])
+			// fmt.Println(tableau[tmp])
+			break
+		}
+
+	}
+
+	//fmt.Println(a)
+
+	aInt, _ := strconv.Atoi(a)
+	fmt.Println(tableau[aInt-1]) // car tableau commence a 0
+
+	for i := 0; i < len(tableau); i++ {
+		if tableau[i] == string("now ") {
+			// fmt.Println(tableau[i-1])
+			for _, aretab := range tableau[i-1] {
+
+				if aretab == 114 {
+
+					aretab = aretab / 38
+
+					fmt.Println(tableau[aretab-1])
+				}
+
+			}
+
+		}
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	fmt.Println("La reponse est : ", rand.Int())
+
+}
